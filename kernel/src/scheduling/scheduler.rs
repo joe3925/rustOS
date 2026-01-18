d7a96c76167ff4691243aa2f276ad9193cf6cdd7
boden1203 <bodenboden212@gmail.com>
2026-01-18 10:50:29 -0800

    bug fix;

--- kernel/src/scheduling/scheduler.rs
+++ kernel/src/scheduling/scheduler.rs
@@ -790,6 +790,7 @@ pub extern "C" fn ipi_eoi_only() {
 #[no_mangle]
 pub extern "win64" fn ipi_entry() {
     naked_asm!(
+        "/* {upper} {lower} {eoi_only} */",
         "cli",
         "push rax",
         "mov  rax, {upper}",
@@ -819,9 +820,17 @@ pub extern "win64" fn ipi_entry() {
 
         "9:",
         "pop  rax",
+        "push r15","push r14","push r13","push r12",
+        "push r11","push r10","push r9","push r8",
+        "push rdi","push rsi","push rbp","push rbx",
+        "push rdx","push rcx","push rax",
         "sub  rsp, 8",
         "call {eoi_only}",
         "add  rsp, 8",
+        "pop  rax","pop  rcx","pop  rdx","pop  rbx",
+        "pop  rbp","pop  rsi","pop  rdi","pop  r8",
+        "pop  r9","pop  r10","pop  r11","pop  r12",
+        "pop  r13","pop  r14","pop  r15",
         "iretq",
 
         upper = const IDLE_UUID_UPPER,
