8b8522fc75f0811fe5c00172abbbc3e054204565
boden1203 <bodenboden212@gmail.com>
2026-04-21 18:02:09 -0700

    fix imports;

--- kernel_api/src/dma.rs
+++ kernel_api/src/dma.rs
@@ -1,26 +1,22 @@
 use alloc::sync::Arc;
 
-pub use kernel_types::dma::{
-    Bidirectional, Described, DmaDeviceHandle, DmaDeviceState, DmaMapped, DmaPciDeviceIdentity,
-    FromDevice, Mdl, MdlDmaSegment, MdlError, MdlPageFrame, Pinned, ToDevice,
-    DMA_IOMMU_VENDOR_AMD_IVRS, DMA_IOMMU_VENDOR_INTEL_DMAR, DMA_IOMMU_VENDOR_NONE,
-    DMA_PCI_IDENTITY_FLAG_BUS_MASTER_CAPABLE, DMA_PCI_IDENTITY_FLAG_BUS_MASTER_ENABLED,
-    MDL_INLINE_PAGE_CAPACITY, MDL_INLINE_SEGMENT_CAPACITY, MDL_PAGE_SIZE,
-};
 use kernel_types::device::DeviceObject;
+pub use kernel_types::dma;
 use kernel_types::status::DriverStatus;
 
 pub fn register_pci_pdo(
     pdo: &Arc<DeviceObject>,
-    identity: DmaPciDeviceIdentity,
+    identity: dma::DmaPciDeviceIdentity,
 ) -> DriverStatus {
     unsafe { kernel_sys::kernel_dma_register_pci_pdo(pdo, identity) }
 }
 
-pub fn open_device_handle(device: &Arc<DeviceObject>) -> Result<DmaDeviceHandle, DriverStatus> {
+pub fn open_device_handle(
+    device: &Arc<DeviceObject>,
+) -> Result<dma::DmaDeviceHandle, DriverStatus> {
     unsafe { kernel_sys::kernel_dma_open_device_handle(device) }
 }
 
-pub fn query_device_state(device: &Arc<DeviceObject>) -> Option<DmaDeviceState> {
+pub fn query_device_state(device: &Arc<DeviceObject>) -> Option<dma::DmaDeviceState> {
     unsafe { kernel_sys::kernel_dma_query_device_state(device) }
 }
